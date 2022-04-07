1db4adabd9f5743fe8b82e328d1a11a044a69d77
KevinGiesberts <kevin.giesberts@gmail.com>
2022-04-07 16:40:43 +0200

    Dammit

---
+++ Constuction/Constuction/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "libraries/include/SFML/Graphics.hpp"
+#include "libraries/include/SFML/System.hpp"
+#include "libraries/include/SFML/Window.hpp"
+#include "libraries/include/SFML/Audio.hpp"
+#include "libraries/include/SFML/Graphics.hpp"
+
+int main()
+{
+	return 0;
+}
