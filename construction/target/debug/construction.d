67df64c4b28d1d15257b325844dfaf2f4c0adcf5
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-12 20:26:00 +0200

    Update

--- construction/target/debug/construction.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Giesberts\Documents\GitHub\Construction-Game\construction\target\debug\construction.exe: C:\Users\Giesberts\Documents\GitHub\Construction-Game\construction\src\main.rs
--- construction/target/debug/deps/construction-d893f39d1485efef.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Giesberts\Documents\GitHub\Construction-Game\construction\target\debug\deps\construction-d893f39d1485efef.exe: src\main.rs
-
-C:\Users\Giesberts\Documents\GitHub\Construction-Game\construction\target\debug\deps\construction-d893f39d1485efef.d: src\main.rs
-
-src\main.rs:
