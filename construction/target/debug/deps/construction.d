9b473510c3ea24736ea72c4a83c5fa1c10ac66ed
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-12 20:26:48 +0200

    Create constructions

---
+++ construction/target/debug/deps/construction.d
@@ -0,0 +1,5 @@
+C:\Users\Giesberts\Documents\GitHub\Construction-Game\construction\target\debug\deps\construction.exe: src\main.rs
+
+C:\Users\Giesberts\Documents\GitHub\Construction-Game\construction\target\debug\deps\construction.d: src\main.rs
+
+src\main.rs:
